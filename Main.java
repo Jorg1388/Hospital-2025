a3fe55aaa2eecfc0b57ea588b5c2d7562e900864
Jorge <jgomez@scl.edu.gt>
2025-05-22 22:06:19 -0600

    modularizacion de panel banner

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
 
 import javax.swing.*;
 
+import vistas.bannerPanel;
 import vistas.loginPanel;
 
 public class Main {
@@ -18,17 +17,16 @@ public class Main {
         miVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         miVentana.setLayout(new BorderLayout());
 
-        //Panel banner
-        JPanel panelBanner = new JPanel();
-        panelBanner.setPreferredSize(new Dimension(500,800));
-        panelBanner.setOpaque(true);
-        panelBanner.setBackground(Color.RED);
-        miVentana.add(panelBanner,BorderLayout.WEST);
-        
         // Panel login
-        miVentana.add(new loginPanel().getPanel(),BorderLayout.CENTER);
+        loginPanel loginPanel = new loginPanel();
+        miVentana.add(loginPanel.getPanel(), BorderLayout.CENTER);
 
         miVentana.setVisible(true);
+        
+        // Panel Banner 
+        bannerPanel BannerPanel = new bannerPanel();
+        miVentana.add(BannerPanel.getBanner(), BorderLayout.WEST);
+
         
     }
 }
