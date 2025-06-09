df6714edcbca7715503800eb85767cd58461618a
Jorge <jgomez@scl.edu.gt>
2025-06-09 17:56:52 -0600

    modulos completados para trabajar

---
+++ data/Pacientes.java
@@ -0,0 +1,21 @@
+package data;
+
+public class Pacientes {
+
+    private Object[][] datos = {
+    {1, "Ana", 20}, {2, "Luis", 22}, {3, "María", 19}, {4, "Carlos", 21}, {5, "Elena", 23},
+    {6, "Pedro", 24}, {7, "Lucía", 20}, {8, "Jorge", 22}, {9, "Laura", 19}, {10, "Diego", 25},
+    {11, "Sofía", 18}, {12, "Daniel", 23}, {13, "Carmen", 21}, {14, "Andrés", 22}, {15, "Valeria", 20},
+    {16, "Hugo", 24}, {17, "Natalia", 19}, {18, "Fernando", 21}, {19, "Clara", 23}, {20, "Iván", 22},
+    {21, "Marta", 20}, {22, "Rubén", 25}, {23, "Julia", 18}, {24, "Raúl", 24}, {25, "Patricia", 19},
+    {26, "Adrián", 21}, {27, "Isabel", 23}, {28, "Sergio", 22}, {29, "Rosa", 20}, {30, "Víctor", 24},
+    {31, "Lola", 19}, {32, "Gonzalo", 21}, {33, "Alicia", 23}, {34, "Tomás", 22}, {35, "Nuria", 20},
+    {36, "Óscar", 25}, {37, "Paula", 18}, {38, "Jaime", 24}, {39, "Beatriz", 19}, {40, "Álvaro", 21},
+    {41, "Teresa", 23}, {42, "Mateo", 22}, {43, "Irene", 20}, {44, "Emilio", 24}, {45, "Cristina", 19},
+    {46, "Fabián", 21}, {47, "Celia", 23}, {48, "Marcos", 22}, {49, "Noelia", 20}, {50, "Bruno", 25}
+};
+
+    public Object [][] getDatos() {
+       return datos;
+    }
+}
